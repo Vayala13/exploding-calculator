3f89c2f46ebf8df525c78973f4f1e3e1217c78c9
Samantha Chavez <chavezsam303@gmail.com>
2025-04-29 15:19:14 -0500

    Trying to fix

--- Action/Operaation2.java
+++ Action/Operaation2.java
@@ -1,12 +1,12 @@
 package Action;
 
-public interface Operation {
+public interface Operation2 {
     double performOp(double num1, double num2);
     double performOp(double num1);
 }
 
 
-class Subtraction implements Operation {
+class Subtraction implements Operation2 {
     @Override
     public double performOp(double num1, double num2) {
         return num1 - num2;
@@ -15,49 +15,49 @@ class Subtraction implements Operation {
     public double performOp(double num1){}
 }
 
-public class Multiplication implements Operation {
+public class Multiplication implements Operation2 {
     @Override
     public double performOp(double num1, double num2) {
         return num1 * num2;
     }
 }
 
-public class Division implements Operation {
+public class Division implements Operation2 {
     @Override
     public double performOp(double num1, double num2) {
         return num1 / num2;
     }
 }
 
-public class Logarithm implements Operation {
+public class Logarithm implements Operation2 {
     @Override
     public double performOp(double num1) {
         return Math.log10(num1);
     }
 }
 
-public class SquareRoot implements Operation {
+public class SquareRoot implements Operation2 {
     @Override
     public double performOp(double num1) {
         return Math.sqrt(num1);
     }
 }
 
-public class Sine implements Operation {
+public class Sine implements Operation2 {
     @Override
     public double performOp(double num1) {
         return Math.sin(num1);
     }
 }
 
-public class Cosine implements Operation {
+public class Cosine implements Operation2 {
     @Override
     public double performOp(double num1) {
         return Math.cos(num1);
     }
 }
 
-public class Tangent implements Operation {
+public class Tangent implements Operation2 {
     @Override
     public double performOp(double num1) {
         return Math.tan(num1);
