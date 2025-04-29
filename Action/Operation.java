82378b5e1f10d5d863bc6c6b43ca136e981bce3e
Samantha Chavez <chavezsam303@gmail.com>
2025-04-29 12:31:05 -0500

    Not done but you can see what I've done so far

--- Action/Operation.java
+++ Action/Operation.java
@@ -1,90 +1,69 @@
-/*  BREAKING DOWN SOME OF THIS CODE
-    
-PACKAGES: 
-    About javax.swing
-        - Main source of GUI
-        - Contains public classes: JTextField, JFrame, JButton, JPanel, SwingUtilities, etc.
-        - Contains packages awt and awt.event
-    About java.awt 
-        - Contains public classes border.BorderLayout, Font, GridLayout, etc. 
-        - Makes things look pretty  
-    About java.awt.event
-        - Contains class: ActionEvent and interface: ActionListener
-        - Makes buttons work 
-
-CLASSES:
-    About JFrame
-        - Contains public methods: setTitle, setSize, setDefaultCloseOperation,
-            setLayout, add, setVisible
-    About ActionEvent
-        - Contains method: getActionCommand 
-    About Double
-        - Contains static method: parseDouble (converts string to double) 
-
-INTERFACES:
-    About ActionListener
-        - Responds to ActionEvent 
-*/ 
-
-// Importing necessary classes to make GUI
 import javax.swing.*; 
 import java.awt.*;  
 import java.awt.event.*;
 
-// cal class inherits JFrame for creating the window && ActionListener for the buttons
-public class Calculator extends JFrame implements ActionListener {
-    // gui displays field where numbers && results are shown
-    private JTextField display;
-    // Variables to store the first && second number && the operation selection
-    private double num1 = 0, num2 = 0;
-    private String operator = "";
-    private boolean isDegree = true; // Starts calc in DEG mode
+public interface Operation {
+    double performOp(double num1, double num2);
+    double performOp(double num1);
+}
 
-    // constructor to set up the GUI
-    public Calculator() {
-        // the title of the window
-        this.setTitle("Calculator");
-        // the size of window 
-        this.setSize(400, 500);
-        // IMPORTANT: makes sure that the program closes
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        // BorderLayout organizes the GUI Components (reason it takes 30 min lol)
-        this.setLayout(new BorderLayout());
+public class Addition implements Operation {
+    public double performOp(double num1, double num2) {
+        return num1 + num2;
+    }
+}
 
-        // create the text field and configure for displayin input && output
-        display = new JTextField();
-        display.setEditable(false);                                 // user wont type directly into the text box
-        display.setFont(new Font("Arial", Font.BOLD, 24));  // text configurations
-        this.add(display, BorderLayout.NORTH);                             // configured to top of window
+public class Subtraction implements Operation {
+    public double performOp(double num1, double num2) {
+        return num1 - num2;
+    }
+}
 
-        // 6 x 4 panel for the buttons n also the spacing
-        JPanel panel = new JPanel(new GridLayout(7, 4, 5, 5));
-        // button labels
-        String[] buttons = {
-            "sin", "cos", "tan", "RAD",
-            "log", "sqrt", "^", "%", 
-            "Del", "(", ")", "/",
-            "7", "8", "9", "x",            
-            "4", "5", "6", "-",
-            "1", "2", "3", "+",
-            "C", "0", ".", "=",
-        };
+public class Multiplication implements Operation {
+    public double performOp(double num1, double num2) {
+        return num1 * num2;
+    }
+}
 
-        // for loop to configure th bottons and andd to the panel
-        for (String text : buttons) {
-            JButton button = new JButton(text);                         // create a button with the label
-            button.setFont(new Font("Arial", Font.BOLD, 20)); // text config
-            button.addActionListener(this);                             // set this to handle clicks
-            panel.add(button);                                          // addin the button to panel line 32 (IF no changes have been made)
-        } 
+public class Division implements Operation {
+    public double performOp(double num1, double num2) {
+        return num1 / num2;
+    }
+}
 
-        // add the panel to the middle (praise be the BorderLayout)
-        this.add(panel, BorderLayout.CENTER);
-        // make it a real boy (papa)
-        this.setVisible(true);
-    } // end of constructor 
-    
-    // Method called anytime a registered JButton (calculator button) variable is pressed 
+public class Logarithm implements Operation {
+    public double performOp(double num1) {
+        return Math.log10(num1);
+    }
+}
+
+public class SquareRoot implements Operation {
+    public double performOp(double num1) {
+        return Math.sqrt(num1);
+    }
+}
+
+public class Sine implements Operation {
+    public double performOp(double num1) {
+        return Math.sin(num1);
+    }
+}
+
+public class Cosine implements Operation {
+    public double performOp(double num1) {
+        return Math.cos(num1);
+    }
+}
+
+public class Tangent implements Operation {
+    public double performOp(double num1) {
+        return Math.tan(num1);
+    }
+}
+
+// ******************************************************** Got this from main. can be deleted
+public class Operation {
+    // handle button clicks
     public void actionPerformed(ActionEvent e) {
         String input = e.getActionCommand();    // get the label of the botton that was pressed
 
@@ -166,4 +145,4 @@ public class Calculator extends JFrame implements ActionListener {
             operator = "";            // clear operators
         }
     }
-}
+};
